b2ff005078b8947b237f58740ced356ea80ff3af
Mathias Ramilo <mathiramilo2290@gmail.com>
2022-10-28 02:16:14 -0300

    Assembly 8086 RPN Calculator

---
+++ obligatorio.asm
@@ -0,0 +1,667 @@
+.data  ; Data segment
+; Default ports
+PORT_LOG_DEFAULT equ 100
+PORT_OUTPUT_DEFAULT equ 200
+; Ports
+PORT_LOG dw PORT_LOG_DEFAULT
+PORT_OUTPUT dw PORT_OUTPUT_DEFAULT
+PORT_INPUT dw 300
+
+; Reserve memory for stack and initialize the base
+; and offset of the software implemented stack
+STACK dw dup(31) 0
+#define SI 0
+; Initializate CX register with 0
+; Will serve as a counter for the number of elements in the stack
+#define CX 0
+
+.code  ; Code segment
+
+main:
+  ; Send 0 to the log port, receive a command
+  ; and send the command to the log port
+  mov AX, 0
+  mov DX, [PORT_LOG]
+  out DX, AX
+  mov DX, [PORT_INPUT]
+  in AX, DX
+  mov DX, [PORT_LOG]
+  out DX, AX
+  ; Switch
+  cmp AX, 1
+  je pushNumber
+  cmp AX, 2
+  je setOutputPort
+  cmp AX, 3
+  je setLogPort
+  cmp AX, 4
+  je top
+  cmp AX, 5
+  je dump
+  cmp AX, 6
+  je duplicate
+  cmp AX, 7
+  je swap
+  cmp AX, 8
+  je negative
+  cmp AX, 9
+  je callFact
+  cmp AX, 10
+  je sum
+  cmp AX, 11
+  je addition
+  cmp AX, 12
+  je substraction
+  cmp AX, 13
+  je multiplication
+  cmp AX, 14
+  je division
+  cmp AX, 15
+  je mod
+  cmp AX, 16
+  je bAnd
+  cmp AX, 17
+  je bOr
+  cmp AX, 18
+  je shiftLeft
+  cmp AX, 19
+  je shiftRight
+  cmp AX, 254
+  je clear
+  cmp AX, 255
+  je halt
+  ; Default (Invalid Command)
+  ; Send code 2 and jump to main
+  mov AX, 2
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+
+pushNumber:
+  mov DX, [PORT_INPUT]
+  in AX, DX
+  mov DX, [PORT_LOG]
+  out DX, AX
+  ; If the stack isn't full, push the number,
+  ; increase CX, send code 16 and jump to main
+  cmp CX, 31
+  jge pnElse
+  add SI, 2
+  mov [STACK + SI], AX
+  inc CX
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+  ; If the stack is full, send code 4 and jump to main
+  pnElse:
+    mov AX, 4
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+
+setOutputPort:
+  ; Recive the param, send it to the log port
+  ; set the output port and send code 16
+  mov DX, [PORT_INPUT]
+  in AX, DX
+  mov DX, [PORT_LOG]
+  out DX, AX
+  mov [PORT_OUTPUT], AX
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+
+setLogPort:
+  ; Recive the param, send it to the log port
+  ; set the log port and send code 16
+  mov DX, [PORT_INPUT]
+  in AX, DX
+  mov DX, [PORT_LOG]
+  out DX, AX
+  mov [PORT_LOG], AX
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+
+top:
+  ; If the stack isn't empty, send the top element to the output port,
+  ; send code 16 and jump to main
+  cmp CX, 0
+  jle topElse
+  mov AX, [STACK + SI]
+  mov DX, [PORT_OUTPUT]
+  out DX, AX
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+  ; If the stack is empty, send code 8 and jump to main
+  topElse:
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+
+dump:
+  ; If the stack isn't empty, send all elements to the output port,
+  ; send code 16 and jump to main
+  cmp CX, 0
+  jle dumpElse
+  mov DI, SI
+  dumpFor:
+    cmp DI, 0
+    jle endDumpFor
+    mov AX, [STACK + DI]
+    mov DX, [PORT_OUTPUT]
+    out DX, AX
+    sub DI, 2
+    jmp dumpFor
+  endDumpFor:
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+  ; If the stack is empty, send code 8 and jump to main
+  dumpElse:
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+
+duplicate:
+  ; If the stack isn't full, duplicate the top of the stack,
+  ; increase CX, send code 16 and jump to main
+  cmp CX, 31
+  jge dupElse
+  mov DX, [STACK + SI]
+  add SI, 2
+  mov [STACK + SI], DX
+  inc CX
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+  ; If the stack is full, send code 4 and jump to main
+  dupElse:
+    mov AX, 4
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+
+swap:
+  ; If the stack has at least 2 elements, swap the top two elements,
+  ; send code 16 and jump to main
+  cmp CX, 2
+  jl swapElse1
+  mov AX, [STACK + SI]
+  sub SI, 2
+  mov DX, [STACK + SI]
+  add SI, 2
+  mov [STACK + SI], DX
+  sub SI, 2
+  mov [STACK + SI], AX
+  add SI, 2
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+  ; If there is 1 element in the stack, remove it, send code 8 and jump to main
+  swapElse1:
+    cmp CX, 1
+    jne swapElse2
+    mov word ptr [STACK + SI], 0
+    sub SI, 2
+    dec CX
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+  ; If there aren't elements in the stack, send code 8 and jump to main
+  swapElse2:
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+
+negative:
+  ; If the stack isn't empty, negate the top element,
+  ; send code 16 and jump to main
+  cmp CX, 0
+  jle negElse
+  mov DX, [STACK + SI]
+  neg DX
+  mov [STACK + SI], DX
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+  ; If the stack is empty, send code 8 and jump to main
+  negElse:
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+
+callFact:
+  ; If the stack isn't empty, calculate the fact of the top element,
+  ; send code 16 and jump to main
+  cmp CX, 0
+  jle callFactElse
+  mov AX, [STACK + SI]
+  call fact
+  mov [STACK + SI], BX
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+  ; If the stack is empty, send code 8 and jump to main
+  callFactElse:
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+
+fact proc
+  ; Factorial using AX as input
+  ; Returns the result on BX
+  cmp AX, 0
+  je isZero
+  dec AX
+  call fact
+  inc AX
+  mov DI, AX
+  mul BX
+  mov BX, AX
+  mov AX, DI
+  jmp endFact
+  isZero:
+  mov BX, 1
+  endFact:
+  ret
+fact endp  
+
+sum:
+  ; Sum all elements of the stack and leave the result in the stack,
+  ; send code 16 and jump to main
+  mov AX, 0
+  mov DI, SI
+  sumFor:
+    cmp DI, 0
+    jle endSumFor
+    add AX, [STACK + DI]
+    sub DI, 2
+    jmp sumFor
+  endSumFor:
+  mov SI, 2
+  mov [STACK + SI], AX
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+
+addition:
+  ; If the stack has more than 2 elements add the top two elements of the stack,
+  ; write the result in the stack, decrease CX, send code 16 and jump to main
+  cmp CX, 2
+  jl addElse1
+  mov DX, [STACK + SI]
+  sub SI, 2
+  mov AX, [STACK + SI]
+  add AX, DX
+  add SI, 2
+  mov word ptr [STACK + SI], 0
+  sub SI, 2
+  mov [STACK + SI], AX
+  dec CX
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+  ; If there is 1 element in the stack, remove it, send code 8 and jump to main
+  addElse1:
+    cmp CX, 1
+    jne addElse2
+    mov word ptr [STACK + SI], 0
+    sub SI, 2
+    dec CX
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+  ; If there aren't elements in the stack, send code 8 and jump to main
+  addElse2:
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+
+substraction:
+  ; If the stack has more than 2 elements subtract the top two elements of the stack,
+  ; write the result in the stack, decrease CX, send code 16 and jump to main
+  cmp CX, 2
+  jl subElse1
+  mov DX, [STACK + SI]
+  sub SI, 2
+  mov AX, [STACK + SI]
+  sub AX, DX
+  add SI, 2
+  mov word ptr [STACK + SI], 0
+  sub SI, 2
+  mov [STACK + SI], AX
+  dec CX
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+  ; If there is 1 element in the stack, remove it, send code 8 and jump to main
+  subElse1:
+    cmp CX, 1
+    jne subElse2
+    mov word ptr [STACK + SI], 0
+    sub SI, 2
+    dec CX
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+  ; If there aren't elements in the stack, send code 8 and jump to main
+  subElse2:
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+
+multiplication:
+  ; If the stack has more than 2 elements multiply the top two elements of the stack,
+  ; write the result in the stack, decrease CX, send code 16 and jump to main
+  cmp CX, 2
+  jl mulElse1
+  mov DX, [STACK + SI]
+  sub SI, 2
+  mov AX, [STACK + SI]
+  imul DX
+  add SI, 2
+  mov word ptr [STACK + SI], 0
+  sub SI, 2
+  mov [STACK + SI], AX
+  dec CX
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+  ; If there is 1 element in the stack, remove it, send code 8 and jump to main
+  mulElse1:
+    cmp CX, 1
+    jne mulElse2
+    mov word ptr [STACK + SI], 0
+    sub SI, 2
+    dec CX
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+  ; If there aren't elements in the stack, send code 8 and jump to main
+  mulElse2:
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+
+division:
+  ; If the stack has more than 2 elements divide the top two elements of the stack,
+  ; write the result in the stack, decrease CX, send code 16 and jump to main
+  cmp CX, 2
+  jl divElse1
+  mov BX, [STACK + SI]
+  sub SI, 2
+  mov AX, [STACK +SI]
+  mov DX, 0
+  idiv BX
+  add SI, 2
+  mov word ptr [STACK +SI], 0
+  sub SI, 2
+  mov [STACK +SI], AX
+  dec CX
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+  ; If there is 1 element in the stack, remove it, send code 8 and jump to main
+  divElse1:
+    cmp CX, 1
+    jne divElse2
+    mov word ptr [STACK + SI], 0
+    sub SI, 2
+    dec CX
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+  ; If there aren't elements in the stack, send code 8 and jump to main
+  divElse2:
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+
+mod:
+  ; If the stack has more than 2 elements calculate the mod of the top two elements of the stack,
+  ; write the result in the stack, decrease CX, send code 16 and jump to main
+  cmp CX, 2
+  jl modElse1
+  mov BX, [STACK + SI]
+  sub SI, 2
+  mov AX, [STACK + SI]
+  mov DX, 0
+  idiv BX
+  add SI, 2
+  mov word ptr [STACK + SI], 0
+  sub SI, 2
+  mov [STACK + SI], DX
+  dec CX
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+  ; If there is 1 element in the stack, remove it, send code 8 and jump to main
+  modElse1:
+    cmp CX, 1
+    jne modElse2
+    mov word ptr [STACK + SI], 0
+    sub SI, 2
+    dec CX
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+  ; If there aren't elements in the stack, send code 8 and jump to main
+  modElse2:
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+
+bAnd:
+  ; If the stack has more than 2 elements calculate the bitwise and of the top two elements of the stack,
+  ; write the result in the stack, decrease CX, send code 16 and jump to main
+  cmp CX, 2
+  jl bAndElse1
+  mov DX, [STACK + SI]
+  sub SI, 2
+  mov AX, [STACK + SI]
+  and AX, DX
+  add SI, 2
+  mov word ptr [STACK + SI], 0
+  sub SI, 2
+  mov [STACK + SI], AX
+  dec CX
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+  ; If there is 1 element in the stack, remove it, send code 8 and jump to main
+  bAndElse1:
+    cmp CX, 1
+    jne bAndElse2
+    mov word ptr [STACK + SI], 0
+    sub SI, 2
+    dec CX
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+  ; If there aren't elements in the stack, send code 8 and jump to main
+  bAndElse2:
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+
+bOr:
+  ; If the stack has more than 2 elements calculate the bitwise or of the top two elements of the stack,
+  ; write the result in the stack, decrease CX, send code 16 and jump to main
+  cmp CX, 2
+  jl bOrElse1
+  mov DX, [STACK + SI]
+  sub SI, 2
+  mov AX, [STACK + SI]
+  or AX, DX
+  add SI, 2
+  mov word ptr [STACK + SI], 0
+  sub SI, 2
+  mov [STACK + SI], AX
+  dec CX
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+  ; If there is 1 element in the stack, remove it, send code 8 and jump to main
+  bOrElse1:
+    cmp CX, 1
+    jne bOrElse2
+    mov word ptr [STACK + SI], 0
+    sub SI, 2
+    dec CX
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+  ; If there aren't elements in the stack, send code 8 and jump to main
+  bOrElse2:
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+
+shiftLeft:
+  ; If the stack has more than 2 elements shift to the left the second element of the stack
+  ; by the number of bits of the first element, write the result in the stack,
+  ; decrease CX, send code 16 and jump to main
+  cmp CX, 2
+  jl shiftLeftElse1
+  mov DX, CX
+  mov CL, [STACK + SI]
+  sub SI, 2
+  mov AX, [STACK + SI]
+  sal AX, CL
+  add SI, 2
+  mov CX, DX
+  mov word ptr [STACK + SI], 0
+  sub SI, 2
+  mov [STACK + SI], AX
+  dec CX
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+  ; If there is 1 element in the stack, remove it, send code 8 and jump to main
+  shiftLeftElse1:
+    cmp CX, 1
+    jne shiftLeftElse2
+    mov word ptr [STACK + SI], 0
+    sub SI, 2
+    dec CX
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+  ; If there aren't elements in the stack, send code 8 and jump to main
+  shiftLeftElse2:
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+
+shiftRight:
+  ; If the stack has more than 2 elements shift to the right the second element of the stack
+  ; by the number of bits of the first element, write the result in the stack,
+  ; decrease CX, send code 16 and jump to main
+  cmp CX, 2
+  jl shiftRightElse1
+  mov DX, CX
+  mov CL, [STACK + SI]
+  sub SI, 2
+  mov AX, [STACK + SI]
+  sar AX, CL
+  add SI, 2
+  mov CX, DX
+  mov word ptr [STACK + SI], 0
+  sub SI, 2
+  mov [STACK + SI], AX
+  dec CX
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+  ; If there is 1 element in the stack, remove it, send code 8 and jump to main
+  shiftRightElse1:
+    cmp CX, 1
+    jne shiftRightElse2
+    mov word ptr [STACK + SI], 0
+    sub SI, 2
+    dec CX
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+  ; If there aren't elements in the stack, send code 8 and jump to main
+  shiftRightElse2:
+    mov AX, 8
+    mov DX, [PORT_LOG]
+    out DX, AX
+    jmp main
+
+clear:
+  ; Remove all elements from the stack, send code 16 and jump to main
+  clearFor:
+  cmp SI, 0
+  jle endClearFor
+  mov word ptr [STACK + SI], 0
+  sub SI, 2
+  jmp clearFor
+  endClearFor:
+  mov CX, 0
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  jmp main
+
+halt:
+  ; Send code 16 and halt the program
+  mov AX, 16
+  mov DX, [PORT_LOG]
+  out DX, AX
+  loopInf:
+  jmp loopInf
+
+.ports
+300: 1,5,1,-10,1,4,14,5,255
+
+.interrupts ; Interruptions handler
+; Interruption example: timer
+;!INT 8 1
+;  iret
+;!ENDINT
---
+++ obligatorio.c
@@ -0,0 +1,548 @@
+#define PORT_LOG_DEFAULT 8;
+#define PORT_OUTPUT_DEFAULT 10;
+#define PORT_INPUT 12;
+
+// Representation of a stack
+short stack[31];
+short stack_amount = 0;
+
+short port_log = PORT_LOG_DEFAULT;
+short port_out = PORT_OUTPUT_DEFAULT;
+short port_input = PORT_INPUT;
+
+int main()
+{
+  short loop = 1;
+  short command;
+  while (loop)
+  {
+    out(port_log, 0);
+    in(command, port_input);
+    out(port_log, command);
+    switch (command)
+    {
+    case 1:
+      pushNumber();
+      break;
+
+    case 2:
+      setOutputPort();
+      break;
+
+    case 3:
+      setLogPort();
+      break;
+
+    case 4:
+      top();
+      break;
+
+    case 5:
+      dump();
+      break;
+
+    case 6:
+      dup();
+      break;
+
+    case 7:
+      swap();
+      break;
+
+    case 8:
+      neg();
+      break;
+
+    case 9:
+      callFact();
+      break;
+
+    case 10:
+      sum();
+      break;
+
+    case 11:
+      add();
+      break;
+
+    case 12:
+      sub();
+      break;
+
+    case 13:
+      mul();
+      break;
+
+    case 14:
+      div();
+      break;
+
+    case 15:
+      mod();
+      break;
+
+    case 16:
+      and();
+      break;
+
+    case 17:
+      or ();
+      break;
+
+    case 18:
+      shiftLeft();
+      break;
+
+    case 19:
+      shiftRight();
+      break;
+
+    case 254:
+      clear();
+      break;
+
+    case 255:
+      halt();
+      break;
+
+    default:
+      // Invalid Command
+      logCode(2);
+      break;
+    }
+  }
+}
+
+// Push a number into the stack
+void pushNumber()
+{
+  short num;
+  in(num, port_input);
+  out(port_log, num);
+  if (stack_amount < 31)
+  {
+    push(num);
+    stack_amount++;
+    logCode(16);
+  }
+  else
+  {
+    logCode(4);
+  }
+  return;
+}
+
+// Set the port for the output
+void setOutputPort()
+{
+  short num;
+  in(num, port_input);
+  port_out = num;
+  out(port_log, num);
+  logCode(16);
+  return;
+}
+
+// Set the port for the log
+void setLogPort()
+{
+  short num;
+  in(num, port_input);
+  port_log = num;
+  out(port_log, num);
+  logCode(16);
+  return;
+}
+
+// Show the top of the stack in the output port
+void top()
+{
+  if (stack_amount > 0)
+  {
+    short num;
+    num = pop();
+    push(num);
+    out(port_out, num);
+    logCode(16);
+  }
+  else
+  {
+    logCode(8);
+  }
+  return;
+}
+
+// Show the stack in the output port
+void dump()
+{
+  if (stack_amount > 0)
+  {
+    short num;
+    for (short i = 0; i < stack_amount; i++)
+    {
+      num = pop();
+      push(num);
+      out(port_out, num);
+    }
+    logCode(16);
+  }
+  else
+  {
+    logCode(8);
+  }
+  return;
+}
+
+// Duplicate the top of the stack
+void dup()
+{
+  if (stack_amount < 31)
+  {
+    short num;
+    num = pop();
+    push(num);
+    push(num);
+    stack_amount++;
+    logCode(16);
+  }
+  else
+  {
+    logCode(4);
+  }
+  return;
+}
+
+// Swap the top of the stack with the second element
+void swap()
+{
+  if (stack_amount >= 2)
+  {
+    short num1, num2;
+    num1 = pop();
+    num2 = pop();
+    push(num1);
+    push(num2);
+    logCode(16);
+  }
+  else if (stack_amount == 1)
+  {
+    pop();
+    stack_amount--;
+    logCode(8);
+  }
+  else
+  {
+    logCode(8);
+  }
+  return;
+}
+
+// Negate the top of the stack
+void neg()
+{
+  if (stack_amount > 0)
+  {
+    short num;
+    num = pop();
+    push(-num);
+    logCode(16);
+  }
+  else
+  {
+    logCode(8);
+  }
+  return;
+}
+
+// Call the factorial function with the top of the stack
+void callFact()
+{
+  if (stack_amount > 0)
+  {
+    short num = pop();
+    short result = fact(num);
+    push(result);
+    logCode(16);
+  }
+  else
+  {
+    logCode(8);
+  }
+}
+
+// Recursive factorial
+short fact(short num)
+{
+  if (num >= 1)
+    return num * fact(num - 1);
+  else
+    return 1;
+}
+
+// Sum all elements of the stack
+void sum()
+{
+  short num, sum = 0;
+  for (short i = 0; i < stack_amount; i++)
+  {
+    num = pop();
+    sum += num;
+  }
+  push(sum);
+  stack_amount = 1;
+  logCode(16);
+  return;
+}
+
+// Add the top of the stack with the second element
+void add()
+{
+  if (stack_amount >= 2)
+  {
+    short num1, num2;
+    num1 = pop();
+    num2 = pop();
+    push(num2 + num1);
+    stack_amount--;
+    logCode(16);
+  }
+  else if (stack_amount == 1)
+  {
+    pop();
+    stack_amount--;
+    logCode(8);
+  }
+  else
+  {
+    logCode(8);
+  }
+  return;
+}
+
+// Substract the second element of the stack with the top
+void sub()
+{
+  if (stack_amount >= 2)
+  {
+    short num1, num2;
+    num1 = pop();
+    num2 = pop();
+    push(num2 - num1);
+    stack_amount--;
+    logCode(16);
+  }
+  else if (stack_amount == 1)
+  {
+    pop();
+    stack_amount--;
+    logCode(8);
+  }
+  else
+  {
+    logCode(8);
+  }
+  return;
+}
+
+// Multiply the second element of the stack with the top
+void mul()
+{
+  if (stack_amount >= 2)
+  {
+    short num1, num2;
+    num1 = pop();
+    num2 = pop();
+    push(num2 * num1);
+    stack_amount--;
+    logCode(16);
+  }
+  else if (stack_amount == 1)
+  {
+    pop();
+    stack_amount--;
+    logCode(8);
+  }
+  else
+  {
+    logCode(8);
+  }
+  return;
+}
+
+// Divide the second element of the stack with the top
+void div()
+{
+  if (stack_amount >= 2)
+  {
+    short num1, num2;
+    num1 = pop();
+    num2 = pop();
+    push(num2 / num1);
+    stack_amount--;
+    logCode(16);
+  }
+  else if (stack_amount == 1)
+  {
+    pop();
+    stack_amount--;
+    logCode(8);
+  }
+  else
+  {
+    logCode(8);
+  }
+  return;
+}
+
+// Mod the second element of the stack with the top
+void mod()
+{
+  if (stack_amount >= 2)
+  {
+    short num1, num2;
+    num1 = pop();
+    num2 = pop();
+    push(num2 % num1);
+    stack_amount--;
+    logCode(16);
+  }
+  else if (stack_amount == 1)
+  {
+    pop();
+    stack_amount--;
+    logCode(8);
+  }
+  else
+  {
+    logCode(8);
+  }
+  return;
+}
+
+// AND the second element of the stack with the top
+void and ()
+{
+  if (stack_amount >= 2)
+  {
+    short num1, num2;
+    num1 = pop();
+    num2 = pop();
+    push(num2 & num1);
+    stack_amount--;
+    logCode(16);
+  }
+  else if (stack_amount == 1)
+  {
+    pop();
+    stack_amount--;
+    logCode(8);
+  }
+  else
+  {
+    logCode(8);
+  }
+  return;
+}
+
+// OR the second element of the stack with the top
+void or ()
+{
+  if (stack_amount >= 2)
+  {
+    short num1, num2;
+    num1 = pop();
+    num2 = pop();
+    push(num2 | num1);
+    stack_amount--;
+    logCode(16);
+  }
+  else if (stack_amount == 1)
+  {
+    pop();
+    stack_amount--;
+    logCode(8);
+  }
+  else
+  {
+    logCode(8);
+  }
+  return;
+}
+
+// Shift left the second element of the stack with top
+void shiftLeft()
+{
+  if (stack_amount >= 2)
+  {
+    short num1, num2;
+    num1 = pop();
+    num2 = pop();
+    push(num2 << num1);
+    stack_amount--;
+    logCode(16);
+  }
+  else if (stack_amount == 1)
+  {
+    pop();
+    stack_amount--;
+    logCode(8);
+  }
+  else
+  {
+    logCode(8);
+  }
+  return;
+}
+
+// Shift right the second element of the stack with the top
+void shiftRight()
+{
+  if (stack_amount >= 2)
+  {
+    short num1, num2;
+    num1 = pop();
+    num2 = pop();
+    push(num2 >> num1);
+    stack_amount--;
+    logCode(16);
+  }
+  else if (stack_amount == 1)
+  {
+    pop();
+    stack_amount--;
+    logCode(8);
+  }
+  else
+  {
+    logCode(8);
+  }
+  return;
+}
+
+// Clear the stack
+void clear()
+{
+  for (short i = 0; i < stack_amount; i++)
+  {
+    pop();
+  }
+  stack_amount = 0;
+  logCode(16);
+  return;
+}
+
+// Halt the program
+void halt()
+{
+  logCode(16);
+  while (1)
+  {
+  }
+}
+
+// Logs
+void logCode(short num)
+{
+  out(port_log, num);
+  return;
+}
